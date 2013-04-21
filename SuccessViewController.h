96577d6d8ef3b3dc7d9120286dec43f7d52f163e
Farhan Abrol <fabrol92@gmail.com>
2013-04-21 19:59:45 -0400

    trying to fix ui

---
+++ SuccessViewController.h
@@ -0,0 +1,13 @@
+//
+//  SuccessViewController.h
+//  taskyhttpdev
+//
+//  Created by Farhan Abrol on 4/21/13.
+//  Copyright (c) 2013 Terrace F. Computer Scientists. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SuccessViewController : UIViewController
+
+@end
